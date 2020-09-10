fe56647fbf649d2815ac0457e1450581f7282307
Steven Gresh <sgresh@yext.com>
2020-09-10 16:19:14 -0400

    Update ghost-nodes.js

--- ghost-nodes.js
+++ ghost-nodes.js
@@ -1,37 +1,31 @@
-const createNodeHelpers = require('gatsby-node-helpers').default;
-const schema = require('./ghost-schema');
+const createNodeHelpers = require("gatsby-node-helpers").default;
+const schema = require("./ghost-schema");
 
-const {
-    createNodeFactory
-} = createNodeHelpers({
-    typePrefix: 'Ghost'
+const { createNodeFactory } = createNodeHelpers({
+  typePrefix: "Ghost"
 });
 
-const POST = 'Post';
-const PAGE = 'Page';
-const TAG = 'Tag';
-const AUTHOR = 'Author';
-const SETTINGS = 'Settings';
+const POST = "Post";
+const PAGE = "Page";
+const TAG = "Tag";
+const SETTINGS = "Settings";
 
 const PostNode = createNodeFactory(POST);
 const PageNode = createNodeFactory(PAGE);
 const TagNode = createNodeFactory(TAG);
-const AuthorNode = createNodeFactory(AUTHOR);
 const SettingsNode = createNodeFactory(SETTINGS);
 
 const fakeNodes = [
-    PostNode(schema.post),
-    PageNode(schema.page),
-    TagNode(schema.tag),
-    AuthorNode(schema.author),
-    SettingsNode(schema.settings)
+  PostNode(schema.post),
+  PageNode(schema.page),
+  TagNode(schema.tag),
+  SettingsNode(schema.settings)
 ];
 
 module.exports = {
-    PostNode,
-    PageNode,
-    TagNode,
-    AuthorNode,
-    SettingsNode,
-    fakeNodes
+  PostNode,
+  PageNode,
+  TagNode,
+  SettingsNode,
+  fakeNodes
 };
